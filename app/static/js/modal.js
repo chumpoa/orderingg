17dc654d98b4f090959670e42433952098c62ded
AgustinLucero11 <aguslucerolp@gmail.com>
2018-04-30 07:46:52 +0200

    modal para editar productos

--- app/static/js/modal.js
+++ app/static/js/modal.js
@@ -3,17 +3,50 @@ const Modal = (function () {
     /**
      * Abre el modal
      **/
-    function open($modal) {
+    function open($modal,caso) {
         const editTitle = document.getElementById('edit-title');
         const saveTitle = document.getElementById('save-title');
         const editButton = document.getElementById('edit-button');
         const saveButton = document.getElementById('save-button');
 
-        $modal.classList.add('is-active');
+        switch(caso) {
+        case "1":                                     //agrega un nuevo producto
 
         editButton.classList.add('is-hidden');
         editTitle.classList.add('is-hidden');
+        saveButton.classList.remove('is-hidden');
+        saveTitle.classList.remove('is-hidden');
+        $modal.classList.add('is-active');
+
+       break;
+
+       case "2":                                    //edita un producto
+        saveButton.classList.add('is-hidden');
+        saveTitle.classList.add('is-hidden');
+        editButton.classList.remove('is-hidden');
+        editTitle.classList.remove('is-hidden');
+        $modal.classList.add('is-active');
+
+           break; }
+
     }
+
+
+    function openEdit($modal) {
+        const editTitle = document.getElementById('edit-title');
+        const saveTitle = document.getElementById('save-title');
+        const editButton = document.getElementById('edit-button');
+        const saveButton = document.getElementById('save-button');
+$modal.classList.add('is-active');
+        editButton.classList.remove('is-hidden');
+        editTitle.classList.remove('is-hidden');
+        saveButton.classList.add('is-hidden');
+        saveButton.classList.add ('is-hidden');
+
+        $modal.classList.add('is-active');
+
+        }
+
 
     /**
      * Cierra el modal
