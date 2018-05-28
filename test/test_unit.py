ad4f68f195123aeff0dc13178a634b17de9e2d1f
Juan Pablo Pardal <juampi@MacBook-Pro-de-Juan.local>
2018-05-27 21:15:13 -0300

    Test de unidad

--- test/test_unit.py
+++ test/test_unit.py
@@ -52,6 +52,53 @@ class OrderingTestCase(TestCase):
         # Verifica que en la lista de productos haya un solo producto
         self.assertEqual(len(p), 1, "No hay productos")
 
+    def test_delete(self):
+        data = {
+            'id':1,
+            'name': 'Tenedor',
+            'price': 50
+        }
+
+        data1 = {
+            'id':2,
+            'name': 'sillon',
+            'price': 500
+        }
+
+        self.client.post('/product', data=json.dumps(data), content_type='application/json')
+        self.client.post('/product', data=json.dumps(data1), content_type='application/json')
+
+        orden = {
+
+            "id" : 1
+        }
+
+        orden = Order()
+        db.session.add(orden)
+        db.session.commit()
+
+        orderProduct =  {"quantity":1,"product":{"id":1}}
+        self.client.post('/order/1/product', data=json.dumps(orderProduct), content_type='application/json')
+        orderProduct =  {"quantity":1,"product":{"id":2}}
+        self.client.post('/order/1/product', data=json.dumps(orderProduct), content_type='application/json')
+
+        cantidad = self.client.get('order/1/product')
+        self.client.delete('/order/1/product/2', data=json.dumps(orderProduct), content_type='application/json')
+        cantidad2 = self.client.get('order/1/product')
+
+
+        self.assertLess(cantidad2,cantidad , "No se borro correctamente el producto")
+
+
+    def test_nombre_vacio(self):
+        producto = {
+            'id': 1,
+            'name': 'jp',
+            'price': 50
+        }
+
+        resp= self.client.post('/product', data=json.dumps(producto), content_type='application/json')
+        assert resp.status_code != 201, "El producto puede ser vacio"
+        
 if __name__ == '__main__':
     unittest.main()
-
